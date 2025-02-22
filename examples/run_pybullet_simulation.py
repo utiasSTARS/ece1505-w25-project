8847ab19547e514f3c19fd0291ad6abaca05231c
Phone Thiha Kyaw <mlsdphonethk@gmail.com>
2025-02-22 17:44:53 -0500

    add example for usage of pybullet env

---
+++ examples/run_pybullet_simulation.py
@@ -0,0 +1,26 @@
+import sys
+import os
+
+# IMPORTANT: allows us to import from our project's modules directly, 
+# even if the script is not run from the project root
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+import simulation.pybullet.env as pyb_env
+
+def main():
+    # create an environment
+    # more enviornment names can be found here:
+    # https://github.com/utiasSTARS/manipulator-learning/tree/master?tab=readme-ov-file#current-environment-list
+    env = pyb_env.create_env("PandaPlayInsertTrayXYZState")
+    # env = pybullet_env.create_env("ThingLiftXYZState")
+    # env = pybullet_env.create_env("PandaReachXYZState", render_opengl_gui=False)
+
+    obs = env.reset()
+    for acts in range(10):
+        next_obs, rew, done, info = env.step(env.action_space.sample())
+        env.render()
+
+    env.close()
+
+if __name__ == "__main__":
+    main()
